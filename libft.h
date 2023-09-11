89c859ec27003b26fc82d5e4e6cc489fbbd63445
Pablo Illescas Ruiz <pillesca@c2r3s2.42malaga.com>
2023-09-11 12:16:15 +0200

    .H y ascii

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pillesca <pillesca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 11:12:49 by pillesca          #+#    #+#             */
+/*   Updated: 2023/09/11 12:14:47 by pillesca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+
+# define LIBFT_H
+
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+
+unsigned long	ft_strlen(char *str);
+
+#endif
